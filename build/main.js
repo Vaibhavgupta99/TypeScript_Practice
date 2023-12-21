a49c9ed20fb94c6d2aa683891db36205f33018aa
Vaibhavgupta99 <Gvaibhav224@gmail.com>
2023-12-21 14:30:01 +0530

    by Vaibhav

---
+++ build/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let a = "hello 1234";
+const names = ["Hello ", "world", 123,];
+/* names.push("Hello") */
+for (var i = 0; i < names.length; i++) {
+    document.write(names[i] + ' ');
+}
+let tuple;
+tuple = [15, 23, true];
+alert(tuple);
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+let a = "hello 1234"
+
+const names = ["Hello ", "world",123 ,]
+
+/* names.push("Hello") */
+for(var i=0; i< names.length; i++ ){
+    document.write(names[i] + ' ')
+}
+
+let tuple: [number, number, boolean]
+
+tuple = [15,23,true]
+
+alert(tuple)
